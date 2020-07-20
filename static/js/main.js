9eb19d796927519ffcdf5288f47da0938863e71c
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2020-07-20 19:56:13 +0000

    add cookie script

--- static/js/main.js
+++ static/js/main.js
@@ -16,6 +16,19 @@ let verifyButtons = document.querySelectorAll('.verify-button')
 for (let button of verifyButtons){
   document.addEventListener('click', function(event){
     let contactDiv = event.target.parentNode.parentNode
+    let contactId = contactDiv.dataset.contactId
+    fetch(`/contacts/${contactId}/verify`, {
+      method:'POST',
+      headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    "X-Requested-With": "XMLHttpRequest",
+    "X-CSRFToken": csrftoken
+  },
+    credentials: 'include'
+    })
+    .then(response => response.json())
+    .then(data => {if (data.verified === 'true')
     
   })
 }
